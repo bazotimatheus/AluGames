0085ecb3c9806f72e0d7c7dc217afbce2ffd5be9
Matheus Bazoti <mbazoti@outlook.com.br>
2024-01-31 16:58:37 -0300

    add list of searched games

--- src/main/kotlin/br/com/alura/alugames/modelo/Gamer.kt
+++ src/main/kotlin/br/com/alura/alugames/modelo/Gamer.kt
@@ -1,5 +1,6 @@
 package br.com.alura.alugames.modelo
 
+import java.util.*
 import kotlin.random.Random
 
 data class Gamer(var nome:String, var email:String) {
@@ -13,8 +14,9 @@ data class Gamer(var nome:String, var email:String) {
             }
         }
 
-    var idInterno:String? = null
-        private set
+    private var idInterno:String? = null
+
+    val jogosBuscados = mutableListOf<Jogo?>()
 
     constructor(nome: String,
                 email: String,
@@ -42,14 +44,14 @@ data class Gamer(var nome:String, var email:String) {
                 "idInterno: " + idInterno + "\n"
     }
 
-    fun criarIdInterno() {
+    private fun criarIdInterno() {
         val numero = Random.nextInt(10000)
         val tag = String.format("%04d", numero)
 
         idInterno = "$usuario#$tag"
     }
 
-    fun validarEmail(): String {
+    private fun validarEmail(): String {
         val regex = Regex("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$")
         if (regex.matches(email)) {
             return email
@@ -57,4 +59,29 @@ data class Gamer(var nome:String, var email:String) {
             throw IllegalArgumentException("Email invalido")
         }
     }
+
+    companion object {
+        fun criarGamer(leitura: Scanner): Gamer {
+            println("Boas vindas ao AluGames! Vamos fazer seu cadastro. Digite seu nome:")
+            val nome = leitura.nextLine()
+            println("Digite seu e-mail:")
+            val email = leitura.nextLine()
+            println("Deseja completar seu cadastro com usuário e data de nascimento? (S/N)")
+            val opcao = leitura.nextLine()
+
+            if (opcao.equals("s", true)) {
+                println("Digite sua data de nascimento(DD/MM/AAAA):")
+                val nascimento = leitura.nextLine()
+                println("Digite seu nome de usuário:")
+                val usuario = leitura.nextLine()
+
+                return Gamer(nome, email, nascimento, usuario)
+            } else {
+                return Gamer(nome, email)
+            }
+
+        }
+    }
+
+
 }
