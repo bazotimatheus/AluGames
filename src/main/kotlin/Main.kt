6470889c66b635860fcd8d714eec7d696e639267
Matheus Bazoti <mbazoti@outlook.com.br>
2024-01-31 09:56:03 -0300

    override toString in Jogo

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -17,4 +17,11 @@ fun main() {
     val json = response.body()
     println(json)
 
+    val meuJogo = Jogo()
+
+    meuJogo.titulo = "Batman: Arkham Asylum Game of the Year Edition"
+    meuJogo.capa = "https:\\\\/\\\\/cdn.cloudflare.steamstatic.com\\\\/steam\\\\/apps\\\\/35140\\\\/capsule_sm_120.jpg?t=1681938587"
+//    meuJogo.descricao = "Jogo do Batman"
+
+    println(meuJogo)
 }
