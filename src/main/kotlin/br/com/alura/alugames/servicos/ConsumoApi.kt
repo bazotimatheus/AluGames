d3cc524e6a415d3bd073e5ea4b003e39101f9456
Matheus Bazoti <mbazoti@outlook.com.br>
2024-02-01 16:22:13 -0300

    added buscaGamers

--- src/main/kotlin/br/com/alura/alugames/servicos/ConsumoApi.kt
+++ src/main/kotlin/br/com/alura/alugames/servicos/ConsumoApi.kt
@@ -1,7 +1,9 @@
 package br.com.alura.alugames.servicos
 
+import br.com.alura.alugames.modelo.InfoGamerJson
 import br.com.alura.alugames.modelo.InfoJogo
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
@@ -36,5 +38,23 @@ class ConsumoApi {
         return meuInfoJogo
     }
 
+    fun buscaGamers(): List<InfoGamerJson> {
+        val endereco = "https://raw.githubusercontent.com/jeniblodev/arquivosJson/main/gamers.json"
+
+        val client: HttpClient = HttpClient.newHttpClient()
+        val request = HttpRequest.newBuilder()
+            .uri(URI.create(endereco))
+            .build()
+        val response = client
+            .send(request, HttpResponse.BodyHandlers.ofString())
+        val json = response.body()
+
+        val gson = Gson()
+
+        val meuGamerTipo = object : TypeToken<List<InfoGamerJson>>() {}.type
+        val listaGamer: List<InfoGamerJson> = gson.fromJson(json, meuGamerTipo)
+
+        return listaGamer
+    }
 
 }
