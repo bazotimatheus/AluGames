5d451ba94018b17403a39f48525b4e55009f689a
Matheus Bazoti <mbazoti@outlook.com.br>
2024-02-06 10:11:10 -0300

    create superclass Plano

--- src/main/kotlin/br/com/alura/alugames/modelo/PlanoAvulso.kt
+++ src/main/kotlin/br/com/alura/alugames/modelo/PlanoAvulso.kt
@@ -1,7 +1,5 @@
 package br.com.alura.alugames.modelo
 
-data class PlanoAvulso(val tipo: String) {
-    fun obterValor(aluguel: Aluguel): Double {
-        return aluguel.jogo.preco * aluguel.periodo.emDias
-    }
+class PlanoAvulso(tipo: String): Plano(tipo) {
+
 }
