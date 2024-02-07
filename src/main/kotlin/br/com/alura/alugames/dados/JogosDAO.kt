defccce9320d12e474916c5e815b2dd3155479e7
Matheus Bazoti <mbazoti@outlook.com.br>
2024-02-07 17:28:27 -0300

    added EntityManager

--- src/main/kotlin/br/com/alura/alugames/dados/JogosDAO.kt
+++ src/main/kotlin/br/com/alura/alugames/dados/JogosDAO.kt
@@ -1,17 +1,19 @@
 package br.com.alura.alugames.dados
 
 import br.com.alura.alugames.modelo.Jogo
+import javax.persistence.EntityManager
 
-class JogosDAO {
-
+class JogosDAO(val manager: EntityManager) {
     fun getJogos(): List<Jogo> {
-        val manager = Banco.getEntityManager()
-        try {
-            val query = manager.createQuery("FROM JogoEntity", JogoEntity::class.java)
-            return query.resultList.map { entity -> Jogo(entity.titulo, entity.capa, entity.preco, entity.descricao, entity.id) }
-        } finally {
-            manager.close()
-        }
+        val query = manager.createQuery("FROM JogoEntity", JogoEntity::class.java)
+        return query.resultList.map { entity -> Jogo(entity.titulo, entity.capa, entity.preco, entity.descricao, entity.id) }
+    }
+
+    fun adicionarJogo(jogo: Jogo) {
+        val entity = JogoEntity(jogo.titulo.toString(), jogo.capa.toString(), jogo.preco.toDouble(), jogo.descricao.toString())
+        manager.transaction.begin()
+        manager.persist(entity)
+        manager.transaction.commit()
     }
 
 //    fun getJogos(): List<Jogo> {
